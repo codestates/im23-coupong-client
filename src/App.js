87b88cc8c477532720c0af32e236247c3b4a5754
WooJunKang <woojiworld@gmail.com>
2020-11-13 00:03:15 +0900

    feat: set state when login submit (#3)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Nav from './component/Nav';
 import { MyPage, Main, Login, SignUp, CouponSetUp } from './pages';
@@ -6,34 +7,51 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route path="/mypage">
-            <MyPage />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route path="/couponsetup">
-            <CouponSetUp />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+// function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      is_login: false
+    };
+  }
+
+  handleLogin() {
+    console.log('haha')
+    this.setState({
+      is_login: !this.state.isLogin
+    })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Router>
+          <Nav isLogin={this.state.is_login} />
+          <Switch>
+            <Route exact path="/">
+              <Main />
+            </Route>
+            <Route path="/mypage">
+              <MyPage />
+            </Route>
+            <Route path="/login">
+              <Login handleLogin={this.handleLogin.bind(this)} />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/couponsetup">
+              <CouponSetUp />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    )
+  };
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import './CSS/Login.css';
+const TEMP_ACCOUNT = {
+  email: 'a@a.a',
+  password: '1234'
+}
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      pw: "",
+      errMsg: ""
+    };
+
+    this.handleEmailInputChange = this.handleEmailInputChange.bind(this);
+    this.handlePwInputChange = this.handlePwInputChange.bind(this);
+    this.userValidCheck = this.userValidCheck.bind(this);
+  }
+
+  handleEmailInputChange(e) {
+
+    this.setState({ email: e.target.value });
+  }
+
+  handlePwInputChange(e) {
+
+    this.setState({ pw: e.target.value });
+  }
+
+  userValidCheck(e) {
+    e.preventDefault();
+    // 정상 로그인
+    if (TEMP_ACCOUNT.email === this.state.email) {
+      if (TEMP_ACCOUNT.password === this.state.pw) {
+        this.props.handleLogin(this.state);
+        this.setState({ errMsg: "" })
+      } else { // 비밀번호가 틀림
+        this.setState({ errMsg: "비밀번호가 일치하지 않습니다." });
+      }
+    } else { // 가입된 유저가 아님
+      this.setState({ errMsg: "가입된 이력이 없습니다." });
+    }
+
+  }
+
+
   render() {
+    console.log(this.state)
     return (
       <div className="login-wrapper">
         <div className="login-container">
@@ -22,16 +68,16 @@ class Login extends React.Component {
 
           {/* login form */}
           <div className="login-form-container">
-            <form className="login-form" autoComplete="off" action="">
+            <form className="login-form" autoComplete="off" onSubmit={this.userValidCheck}>
               <div className="login-group">
                 <label type="email">Email:</label>
-                <input type="email" id="email-input" name="email" />
+                <input type="email" id="email-input" name="email" value={this.state.email} onChange={this.handleEmailInputChange} />
               </div>
               <div className="login-group">
                 <label type="password">Password:</label>
-                <input type="password" id="password-input" name="password" />
+                <input type="password" id="password-input" name="password" value={this.state.pw} onChange={this.handlePwInputChange} />
               </div>
-              <a href="/" class="forget-pw-link">Forgot password?</a>
+              <a href="/" className="forget-pw-link">Forgot password?</a>
               <input type="submit" value="Login" id="login-submit" />
             </form>
           </div>
