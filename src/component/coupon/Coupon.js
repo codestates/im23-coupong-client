6c040c4cb687664d9c76fbbd12e5a1ab4d8a9407
WooJunKang <woojiworld@gmail.com>
2020-11-18 01:12:27 +0900

    feat: coupon UI component & click action for more btn (#5)

---
+++ src/component/coupon/Coupon.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import IMG from '../../images/coupon_template_1.png';
+import ScaleText from "react-scale-text";
+
+class Coupon extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return (
+      <div className="bg-container" style={{ backgroundImage: `url(${IMG})` }}>
+        <div className="bg-text-container parent" style={{ width: "100%", height: "6rem" }}>
+          <ScaleText>
+            <p className="child"> {this.props.title ? this.props.title : "제목을 입력해 주세요"} </p>
+          </ScaleText>
+        </div>
+
+        <div className="bg-desc-container" style={{ width: "100%", height: "6rem" }}>
+          <div className="bg-clock-container">
+            <img src="https://image.flaticon.com/icons/png/512/223/223404.png"
+              alt="clock"
+            />
+          </div>
+          <div className="bg-date-container">
+            <label type="start-date">Start Date:</label>
+            <input readOnly id="start-date" value={this.props.startDate} />
+            <label type="end-date">End Date:</label>
+            <input readOnly id="end-date" value={this.props.endDate} />
+          </div>
+        </div>
+      </div>
+    )
+  };
+}
+export default Coupon;
+
+
+
--- src/pages/CouponSetUp.js
+++ src/pages/CouponSetUp.js
@@ -1,11 +1,114 @@
 import React from 'react';
+import Coupon from '../component/coupon/Coupon';
+import DatePicker from 'react-date-picker';
+import './CSS/CouponSetUp.css';
+
+
 
 class CouponSetUp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      coupon_cnt: 1,
+      coupon_data: [
+        {
+          no: 1,
+          title: "",
+          start_date: "",
+          end_date: ""
+        }
+      ],
+      errMsg: ""
+    }
+
+    this.handleMoreBtn = this.handleMoreBtn.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleMoreBtn() {
+    console.log(this.state);
+    if (this.state.coupon_cnt === 3) {
+      return this.setState({ errMsg: "쿠폰 추가 최대갯수는 3개입니다." })
+    } else {
+      // this.setState({ coupon_cnt: prevCnt + 1 })
+      return this.setState((state) => {
+        return {
+          coupon_cnt: state.coupon_cnt + 1,
+          coupon_data: [...state.coupon_data, {
+            no: state.coupon_cnt + 1,
+            title: "",
+            start_date: "",
+            end_date: ""
+          }]
+        }
+      })
+    }
+  }
+
+  handleInputChange = (key, num) => (event) => {
+
+    let items = [...this.state.coupon_data];
+    let item = { ...items[num - 1] };
+    if (key === "title") {
+      item[key] = event.target.value;
+    } else {
+      item[key] = event
+    }
+    items[num - 1] = item;
+    this.setState({ coupon_data: items })
+
+  }
+
+  // this.setState((state, props) => {
+  //   return {counter: state.counter + props.step};
+  // });
   render() {
     return (
       // 여기에 couponEditor comp들어와야됨
-      <div>
-        this is CouponSetUp !
+      <div className="coupon-editor-container">
+        <section className="editor-viewer-section">
+          <div className="viewer-display">
+
+            {this.state.coupon_data.map(coupon => {
+              return (
+                <Coupon no={coupon.no}
+                  title={coupon.title}
+                  startDate={coupon.start_date}
+                  endDate={coupon.end_date}
+                />
+              )
+            })}
+
+            {this.state.errMsg && (alert(this.state.errMsg))}
+
+          </div>
+          <div className="viewer-btn-container">
+            <button className="viewer-more-btn" onClick={this.handleMoreBtn}>+</button>
+          </div>
+        </section>
+        <section className="editor-write-section">
+
+          <div className="write-container no-1">
+            <h5 className="coupon-no">#1</h5>
+            <label type="coupon-title">Title:</label>
+            <input type="text"
+              onChange={this.handleInputChange("title", 1)}
+              value={this.state.coupon_data[0].title}
+            />
+            <label type="coupon-start-date">Start Date:</label>
+            <DatePicker
+              onChange={this.handleInputChange("start_date", 1)}
+              value={this.state.coupon_data[0].start_date}
+            />
+            <label type="coupon-end-date">End Date:</label>
+            <DatePicker
+              onChange={this.handleInputChange("end_date", 1)}
+              value={this.state.coupon_data[0].end_date}
+            />
+          </div>
+
+        </section>
       </div>
     );
   }
